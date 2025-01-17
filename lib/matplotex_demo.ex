36eac30ff2af65e9c872b9dd173f210448a6b140
Mohammed Sadique <msadique@boweryfarming.com>
2025-01-17 15:45:17 +0530

    sample

---
+++ lib/matplotex_demo.ex
@@ -0,0 +1,18 @@
+defmodule MatplotexDemo do
+  @moduledoc """
+  Documentation for `MatplotexDemo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MatplotexDemo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/matplotex_demo_test.exs
@@ -0,0 +1,8 @@
+defmodule MatplotexDemoTest do
+  use ExUnit.Case
+  doctest MatplotexDemo
+
+  test "greets the world" do
+    assert MatplotexDemo.hello() == :world
+  end
+end
