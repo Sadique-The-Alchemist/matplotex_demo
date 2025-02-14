e16865c0c5dc67bbe7c58a64f24a52157ab63537
Sadique-The-Alchemist <sadique.pal@gmail.com>
2025-02-14 20:00:40 +0530

    version update

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule MatplotexDemo.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:matplotex, path: "../matplotex"}
+      {:matplotex, "~> 0.4.71"}
     ]
   end
 end
